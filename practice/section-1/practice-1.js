865eb45007311ce7c1974fffca7cf1254a80ffe7
duanyiran <dyr6@qq.com>
2019-08-19 18:44:34 +0800

    作业-未完成

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -1,5 +1,12 @@
 'use strict';
 
 function collectSameElements(collectionA, collectionB) {
-  return '实现练习要求，并改写该行代码。';
+  var newArr = []{
+  for (var i=0,i<collectionA.length,i++);
+    for (var j=0,i<collectionB.length,i++);
+      }
+    if(collectionA[i]=collectionB[j]);
+    newArr.push(j)
+
+  return newArr;
 }
